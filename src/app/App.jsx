6295166af864f1102fb04971e7c713c0d0ae5379
sania-rose-boby <saniaroseboby@gmail.com>
2026-01-11 11:24:40 +0530

    first commit

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import Level0 from "./screens/Level0/Level0";
+import "./styles/global.css";
+import "./styles/accessibility.css";
+
+function App() {
+  return (
+    <div className="app-root">
+      <Level0 />
+    </div>
+  );
+}
+
+export default App;
