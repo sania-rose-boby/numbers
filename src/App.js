c329826d15ebf8cb52f5dcf2368dea31a0693d90
sania-rose-boby <saniaroseboby@gmail.com>
2026-01-11 11:26:05 +0530

    Add initial project structure and components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Level0 from "./screens/Level0/Level0";
+import "./styles/global.css";
+import "./styles/accessibility.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-root">
+      <Level0 />
     </div>
   );
 }
